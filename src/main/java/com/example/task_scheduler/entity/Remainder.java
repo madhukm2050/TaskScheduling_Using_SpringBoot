7e072b5d77cf5abbfd1d49bad0d457e1d81679c6
madhu <madhukm2050@gmail.com>
2025-12-29 16:39:33 +0530

    Created remainder entity

---
+++ src/main/java/com/example/task_scheduler/entity/Remainder.java
@@ -0,0 +1,59 @@
+package com.example.task_scheduler.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Remainder {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String email;
+    private String message;
+    private LocalDateTime scheduledTime;
+    private boolean sent;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getScheduledTime() {
+        return scheduledTime;
+    }
+
+    public void setScheduledTime(LocalDateTime scheduledTime) {
+        this.scheduledTime = scheduledTime;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
+    }
+}
