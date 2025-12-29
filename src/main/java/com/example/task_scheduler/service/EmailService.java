ff64df2b5e8a34b5380ed23f5007ddfe88acd82f
madhu <madhukm2050@gmail.com>
2025-12-29 17:33:37 +0530

    Added the Mail service

---
+++ src/main/java/com/example/task_scheduler/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.example.task_scheduler.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject, String body){
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo(to);
+        msg.setSubject(subject);
+        msg.setText(body);
+        javaMailSender.send(msg);
+    }
+
+
+}
