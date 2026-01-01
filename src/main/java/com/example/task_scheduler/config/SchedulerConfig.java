ff0114aebf63ae8565d31cef06f798da95c2ab6b
madhu <madhukm2050@gmail.com>
2026-01-01 22:28:40 +0530

    Updated

--- src/main/java/com/example/task_scheduler/config/SchedulerConfig.java
+++ src/main/java/com/example/task_scheduler/config/SchedulerConfig.java
@@ -10,7 +10,7 @@ public class SchedulerConfig {
     public ThreadPoolTaskScheduler taskScheduler(){
         ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
         scheduler.setPoolSize(5);
-        scheduler.setThreadNamePrefix("Task Scheduler");
+        scheduler.setThreadNamePrefix("Task Scheduler - ");
         scheduler.initialize();
         return scheduler;
     }
