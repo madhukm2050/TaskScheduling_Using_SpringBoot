57b50361b5cfc6d8afe831677f5e933abd038d4e
madhu <madhukm2050@gmail.com>
2026-01-02 20:32:15 +0530

    added the table name

--- src/main/java/com/example/task_scheduler/entity/Remainder.java
+++ src/main/java/com/example/task_scheduler/entity/Remainder.java
@@ -3,10 +3,12 @@ package com.example.task_scheduler.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "reminders")
 public class Remainder {
     @Id
     @GeneratedValue
